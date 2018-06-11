27c6559b0d2c1457fcba73c5443e0662e29a9b6f
plcp <matthieu@daumas.me>
2018-06-11 19:21:18 +0200

    Cleanup + Add some comments

--- circuit_ntor.py
+++ circuit_ntor.py
@@ -1,5 +1,4 @@
 import base64
-import collections
 
 import stem
 import stem.client.cell
@@ -10,6 +9,8 @@ import ntor_ref
 import consensus
 
 def create(link, identity, onion_key, circuits=[], sanity=True):
+
+    # Expect the hash of node's identity as 20 bytes or as some base64
     try:
         if isinstance(identity, str):
             identity = base64.b64decode(identity + '====')
@@ -17,6 +18,7 @@ def create(link, identity, onion_key, circuits=[], sanity=True):
     except BaseException:
         pass
 
+    # Expect the node's ed25519 ntor-onion-key as 32 bytes or as some base64
     try:
         if isinstance(onion_key, str):
             onion_key = base64.b64decode(onion_key + '====')
@@ -24,29 +26,36 @@ def create(link, identity, onion_key, circuits=[], sanity=True):
     except BaseException:
         pass
 
+    # ¹(see create_fast.py for details on redundant parts)
     link_socket, link_version = link
 
-    circuit_id = 0x80000000 if link_version > 3 else 0x0001 # handle MSB (see¹)
+    # (pick an available circuit_id)¹
+    circuit_id = 0x80000000 if link_version > 3 else 0x0001
     while circuit_id in circuits:
         circuit_id += 1
 
+    # (extra useless checks)¹
     if sanity:
         if link_version > 3 and not (0x100000000 > circuit_id > 0):
             return None, None
         if link_version < 4 and not (0x10000 > circuit_id > 0):
             return None, None
 
+    # Perform the first part of our handshake
     donna_onion_key = curve25519.keys.Public(onion_key)
     ephemeral_key, payload = ntor_ref.client_part1(identity, donna_onion_key)
 
+    # Build a CREATE2 cell containing this first handshake part
     create_scell = stem.client.cell.Create2Cell(circuit_id, hdata=payload)
     link_socket.send(create_scell.pack(link_version))
 
+    # Send the cell
     answer = link_socket.recv()
     if not answer:
         return None, None
     created_rcell = None
 
+    # (retrieve the cell)¹
     if sanity:
         created_rcell, _ = stem.client.cell.Cell.pop(answer, link_version)
         if created_rcell.circ_id != circuit_id:
@@ -63,6 +72,9 @@ def create(link, identity, onion_key, circuits=[], sanity=True):
             return None, None
         created_rcell = created_rcells[0] # (expecting only one CREATED2)
 
+    # After receiving the 2nd part of the handshake, finish it on our side...
+    #   ...enabling us to retrieve shared (derived) key material.
+    #
     raw_material = ntor_ref.client_part2(ephemeral_key, created_rcell.data,
 identity, donna_onion_key, keyBytes=92)
 
@@ -97,37 +109,38 @@ if __name__ == '__main__':
     link = link_protocol.handshake(address=sys_argv.addr, port=sys_argv.port)
     print('Link v{} established – {}'.format(link[1], link[0]))
 
-    # create a single-hop fast circuit to access the directory through it
+    # Create a single-hop fast circuit to access the directory through it
     circuit = circuit_fast.create(link)
     print('Circuit {} created – Key hash: {}'.format(circuit[0],
         circuit[1].key_hash.hex()))
 
-    # get first hop descriptor
+    # Download our OR's descriptor
     state = onion_parts.state(link, circuit)
     state, last_stream_id, authority = descriptors.download_authority(state)
 
-    # find our first hop into the consensus – TODO: validate signatures
+    # Download an unflavored consensus
     state, last_stream_id, consensus = consensus.download(state,
         flavor='unflavored', last_stream_id=last_stream_id)
 
+    # Find our OR into the consensus – TODO: validate all the signatures
     for router in consensus['routers']:
         if router['digest'] == authority['digest']:
             break
 
-    # create a single-hop ntor-ish circuit (only do that to showcase CREATE2)
-    circuit_ntor = create(
-        link,
-        router['identity'],
-        authority['ntor-onion-key'],
+    # Create a ntor-ish circuit (single-hop, only to showcase the API)
+    circuit_ntor = create(link,
+        router['identity'], authority['ntor-onion-key'],
         [circuit[0]])
+
+    # It's working?
     print('Circuit {} created – Key hash: {}'.format(circuit_ntor[0],
         circuit_ntor[1].key_hash.hex()))
 
-    # retrieve again first hop's descriptor to check if the circuit works
+    # Retrieve again first hop's descriptor to check if the circuit works
     state_ntor = onion_parts.state(link, circuit_ntor)
     state_ntor, lsid, authority = descriptors.download_authority(state_ntor)
 
-    # it works!
+    # It is working!
     assert authority['digest'] == router['digest']
     print(('\nDirectory successfully reached through ntor-created circuit {}!'
         ).format(circuit_ntor[0]))
