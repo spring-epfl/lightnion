d3c6b726b611f28b44ea91f1845fdd93eecce969
plcp <matthieu@daumas.me>
2018-07-24 18:12:23 +0200

    Add cell.certs module docstring draft

--- ltor/cell/certs.py
+++ ltor/cell/certs.py
@@ -1,6 +1,69 @@
 import cell as _cell
 import cell.view as _view
 
+"""Tor CERTS cells views & wrappers
+
+This module contains views and wrappers to help manipulation of CERTS cell.
+
+It exposes:
+
+    - cell -- The main public class for abstracting CERTS cells. After being
+        constructed with a given raw bytes representation, it gives access to
+        functions
+
+This module
+
+The following is a simple usage example that wraps bytes as a CERTS cell,
+retrieve the third certificate, write it to file and then remove the last
+certificate from the payload:
+
+    certs_cell = cell.certs.cell(peer.recv())
+    if not certs_cell.valid:
+        raise RuntimeError
+
+    if len(certs_cell.certs.listing) < 3:
+        raise RuntimeError
+
+    cert = certs_cell.certs.listing[2]
+    with open('cert', 'wb') as f:
+        f.write(cert.data)
+    print('{} bytes written ({} cert)'.format(cert.clen, cert.type))
+
+    certs_cell.certs.quantity -= 1
+    certs_cell.truncate()
+
+:
+
+    -
+
+    --------------------
+    CERTS cell hierarchy
+    --------------------
+
+        [cell.certs.view]
+            |
+            + header        [cell.header_variable_view]
+            |   + circid        [view.uint(2)]
+            |   + cmd           [cell.cmd]
+            |   + length        [view.cache(view.uint(2))]
+            |
+            + certs         [cell.certs.certs_view]
+                |
+                + header        [cell.certs.certs_header_view]
+                |   + quantity      [view.cache(view.uint(1))]
+                |
+                + listing       [view.series(n=quantity)]
+                    |
+                    + 0 ... n       [cell.certs.cert_view]
+                        |
+                        + header        [cell.certs.cert_header_view]
+                        |   + type          [cell.certs.cert_type]
+                        |   + clen          [view.cache(view.uint(2))]
+                        |
+                        + data          [view.data(size=clen)]
+
+"""
+
 class cert_type(_view.enum(1)):
     RSA_SIGNED_LINK_KEY         = 0x01
     RSA_SELF_SIGNED_IDENTITY    = 0x02
