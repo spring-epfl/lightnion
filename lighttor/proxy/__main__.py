15478dc55ced716606c771afafb133c493a149d3
plcp <matthieu@daumas.me>
2018-08-09 12:44:36 +0200

    Add forward proxy argparse + slave/bootstrap nodes

--- lighttor/proxy/__main__.py
+++ lighttor/proxy/__main__.py
@@ -1,4 +1,43 @@
 import lighttor.proxy.forward
+import argparse
+import ipaddress
+
+def _validate_host(addr):
+    if not (addr.count(':') == 1):
+        raise RuntimeError('Invalid [ip:port] format: {}'.format(addr))
+
+    addr, port = addr.split(':')
+    if not str(ipaddress.IPv4Address(addr)) == addr:
+        raise RuntimeError('Invalid format for IPv4 address: {}'.format(addr))
+
+    if not str(int(port)) == port:
+        raise RuntimeError('Invalid format for port: {}'.format(port))
+
+    return addr, int(port)
 
 if __name__ == '__main__':
-    lighttor.proxy.forward.main()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', type=int, required=False, default=4990,
+        metavar='port', help='Listen to port for HTTP requests.'
+        + ' (default: 4990)')
+    parser.add_argument('-b', required=False, default='127.0.0.1:5000',
+        metavar='ip:or_port', help='Tor node for first consensus.'
+        + ' (default: 127.0.0.1:5000)')
+    parser.add_argument('-s', required=False, default='127.0.0.1:8000',
+        metavar='ip:control_port', help='Slave node for path selection.'
+        + ' (default: 127.0.0.1:8000)')
+    parser.add_argument('--purge-cache', action='store_true',
+        help='If specified, purge cache before starting.')
+    parser.add_argument('--spawn-slave', action='store_true',
+        help='If specified, ignore -s and spawn local slave.')
+
+    argv = parser.parse_args()
+    argv.b = _validate_host(argv.b)
+    argv.s = _validate_host(argv.s)
+
+    lighttor.proxy.forward.main(
+        port=argv.p,
+        slave_node=argv.s if not argv.spawn_slave else None,
+        bootstrap_node=argv.b,
+        purge_cache=argv.purge_cache)
--- lighttor/proxy/forward.py
+++ lighttor/proxy/forward.py
@@ -1,12 +1,67 @@
 import threading
 import flask
+import time
+
+import lighttor as ltor
+import lighttor.proxy
+
+refresh_timeout = 5
 
 class clerk(threading.Thread):
-    def __init__(self):
+    def __init__(self, slave_node, bootstrap_node):
         super().__init__()
+
         self.lock = threading.RLock()
         self.dead = False
         self.tick = 0
+
+        self.bootstrap_node = bootstrap_node
+        self.slave_node = slave_node
+        self.producer = None
+        self.bootlink = None
+
+        self.refresh_producer()
+        self.refresh_bootlink()
+
+    def die(self, e):
+        self.dead = True
+        raise e
+
+    def refresh_producer(self):
+        if self.producer is not None:
+            self.producer.close()
+
+            for _ in range(refresh_timeout):
+                if self.producer.dead:
+                    break
+                time.sleep(1)
+
+            if not self.producer.dead:
+                self.die(
+                    RuntimeError('Unable to kill path emitter, aborting!'))
+
+        if self.slave_node is None:
+            self.producer = ltor.proxy.path.fetch()
+        else:
+            addr, port = self.slave_node
+            self.producer = ltor.proxy.path.fetch(
+                tor_process=False, control_host=addr, control_port=port)
+
+    def refresh_bootlink(self):
+        if self.bootlink is not None:
+            self.bootlink.close()
+
+            for _ in range(refresh_timeout):
+                if self.bootlink.io.dead:
+                    break
+                time.sleep(1)
+
+            if not self.bootlink.io.dead:
+                self.die(
+                    RuntimeError('Unable to close bootstrap link, aborting!'))
+
+        addr, port = self.bootstrap_node
+        self.bootlink = ltor.link.initiate(address=addr, port=port)
 
     def __enter__(self):
         with self.lock:
@@ -34,8 +89,12 @@ app = flask.Flask(__name__)
 @app.route('/')
 def index():
     with app.clerk.lock:
-        return "Hello, World! {}".format(app.clerk.tick)
+        path = app.clerk.producer.get()
+        return "Hello, World! {} {}".format(app.clerk.tick, path)
 
-def main(port=4990):
-    with clerk() as app.clerk:
-        app.run(port=4990, debug=True)
+def main(port, slave_node, bootstrap_node, purge_cache):
+    if purge_cache:
+        ltor.cache.purge()
+
+    with clerk(slave_node, bootstrap_node) as app.clerk:
+        app.run(port=port, debug=True)
