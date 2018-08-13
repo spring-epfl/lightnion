d43423ede9e51624d331b1a489050e64ae16e29c
plcp <matthieu@daumas.me>
2018-08-13 14:52:48 +0200

    Now http.client retries few times upon 503

--- lighttor/http/__init__.py
+++ lighttor/http/__init__.py
@@ -13,17 +13,27 @@ class channel(collections.namedtuple('channel',
     ['id', 'guard', 'middle', 'exit'])):
     pass
 
-def client(host, port=80, *, io, prefix='http', **kwargs):
+def client(host, port=80, *, io, prefix='http', failures=5, **kwargs):
     base_url = '{}://{}:{}{}'.format(prefix, host, port, ltor.proxy.base_url)
 
     guard = None
     try:
         code = 503
-        while code == 503:
+        fails = 0
+        for _ in range(failures):
             rq = requests.get(base_url + '/guard')
             if not rq.status_code in [200, 503]:
                 raise RuntimeError('Error code: {}'.format(rq.status_code))
+
             code = rq.status_code
+            if fails > failures or not code == 503:
+                break
+
+            fails += 1
+
+        if code == 503:
+            raise RuntimeError('Too many failures!')
+
         guard = json.loads(rq.text)
 
     except BaseException as e:
@@ -33,16 +43,25 @@ def client(host, port=80, *, io, prefix='http', **kwargs):
 
     handshake, material, uid, path = (None, None, None, None)
     try:
-        handshake, material = ntor.hand(guard)
-        data = json.dumps(dict(ntor=handshake))
-
         code = 503
-        while code == 503:
+        fails = 0
+        for _ in range(failures):
+            handshake, material = ntor.hand(guard)
+            data = json.dumps(dict(ntor=handshake))
+
             rq = requests.post(base_url + '/channels',
                 data=data, headers=headers)
             if not rq.status_code in [201, 503]:
                 raise RuntimeError('Error code: {}'.format(rq.status_code))
+
             code = rq.status_code
+            if fails > failures or not code == 503:
+                break
+
+            fails += 1
+
+        if code == 503:
+            raise RuntimeError('Too many failures!')
 
         data = json.loads(rq.text)
         uid, handshake, path = data['id'], data['ntor'], data['path']
@@ -54,7 +73,10 @@ def client(host, port=80, *, io, prefix='http', **kwargs):
 
     state = None
     try:
-        handshake = ntor.shake(handshake, material)
+        try:
+            handshake = ntor.shake(handshake, material)
+        except TypeError:
+            raise RuntimeError('Invalid ntor cryptographic material?')
 
         io = io(endpoint=base_url + '/channels/' + uid, **kwargs)
         link = ltor.link.link(io, version='http')
@@ -74,7 +96,7 @@ def client(host, port=80, *, io, prefix='http', **kwargs):
     except BaseException as e:
         raise RuntimeError(
             'Unable extend the circuit to middle node {}, reason: {}'.format(
-                path[0]['router']['nickname'], io, e))
+                path[0]['router']['nickname'], e))
 
     try:
         state = ltor.extend.circuit(state, path[1])
@@ -82,6 +104,6 @@ def client(host, port=80, *, io, prefix='http', **kwargs):
     except BaseException as e:
         raise RuntimeError(
             'Unable extend the circuit to exit node {}, reason: {}'.format(
-                path[0]['router']['nickname'], io, e))
+                path[0]['router']['nickname'], e))
 
     return state, channel(uid, guard, path[0], path[1])
