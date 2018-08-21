f3909ae6c2f4ec729e5dbdf8f6c023bd7b11cd89
plcp <matthieu@daumas.me>
2018-08-21 12:20:48 +0200

    Add lighttor.{enc,dec}.binary util

--- js-client/src/util.js
+++ js-client/src/util.js
@@ -2,6 +2,13 @@ lighttor.enc = {}
 lighttor.enc.bits = sjcl.codec.bytes.toBits
 lighttor.enc.utf8 = nacl.util.encodeUTF8
 lighttor.enc.base64 = nacl.util.encodeBase64
+lighttor.enc.binary = function(data)
+{
+    var str = ""
+    for(var idx = 0; idx < data.length; idx++)
+        str += String.fromCharCode(data[idx])
+    return str
+}
 
 lighttor.dec = {}
 lighttor.dec.bits = function(data)
@@ -10,3 +17,10 @@ lighttor.dec.bits = function(data)
 }
 lighttor.dec.utf8 = nacl.util.decodeUTF8
 lighttor.dec.base64 = nacl.util.decodeBase64
+lighttor.dec.binary = function(str)
+{
+    var data = new Uint8Array(str.length)
+    for(var idx = 0; idx < str.length; idx++)
+        data[idx] = str.charCodeAt(idx)
+    return data
+}
