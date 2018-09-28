72f277b76bbb89f708066fa295d1571e06c75c8c
somecookie <ricardo.ferreiraribeiro@epfl.ch>
2018-09-28 22:55:38 +0200

    Update install-vagrant.sh and VagrantFile

--- Vagrantfile
+++ Vagrantfile
@@ -5,6 +5,8 @@ Vagrant.configure("2") do |config|
     torMachine.vm.network "forwarded_port", guest: 4990, host: 4990 
     torMachine.vm.network "forwarded_port", guest: 8765, host: 8765
 
+   torMachine.vm.provision "shell", path: "install-vagrant.sh" 
+
     torMachine.vm.provider "virtualbox" do |v|
       v.memory = 4096
       v.cpus = 2
--- install-vagrant.sh
+++ install-vagrant.sh
@@ -1,13 +1,80 @@
-apt-get install gnupg2
-gpg2 --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
-gpg2 --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
+#!/bin/bash
 
-# Put into /etc/apt/sources.list.d/
-(cat <<EOF
+echo "Install-vagrant.sh starts running"
+
+APTGET="sudo DEBIAN_FRONTEND=noninteractive apt-get"
+APTGETINSTALL="$APTGET -y install"
+PIP="sudo -H pip3"
+PIPINSTALL="$PIP install"
+
+echo "Updating the packages"
+$APTGET update
+
+echo "Installing gnupg2"
+$APTGETINSTALL gnupg2
+gpg2 --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
+gpg2 --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+
+echo "Put sources into /etc/apt/sources.list.d"
+sudo tee -a /etc/apt/sources.list.d/tor.list << END
 deb https://deb.torproject.org/torproject.org bionic main
 deb-src https://deb.torproject.org/torproject.org bionic main
-EOF
-) > /etc/apt/sources.list.d/tor.list
-apt-get update
+END
 
-apt-get install python3 virtualenv tor python3-pip openjdk-11-jre
+
+$APTGET update
+$APTGET upgrade
+
+if ! hash python 2>/dev/null; then
+	echo "Installing python"
+	$APTGETINSTALL python
+else
+	echo "Python is already installed"
+fi
+
+if ! hash python3 2>/dev/null; then
+	echo "Installing python3"
+	$APTGETINSTALL python3
+else
+	echo "Python3 is already installed"
+fi
+
+if ! hash pip3 2>/dev/null; then
+	echo "Installing pip"
+	$APTGETINSTALL python3-pip
+	$PIPINSTALL --upgrade pip
+else
+	echo "Pip is already installed"
+fi
+
+if ! hash git 2>/dev/null; then
+	echo "Installing git"
+	$APTGETINSTALL git
+else
+	echo "Git is already installed"
+fi
+
+if ! hash virtualenv 2>/dev/null; then
+	echo "Installing virtualenv"
+	$APTGETINSTALL virtualenv
+else
+	echo "Virtualenv is already installed"
+fi
+
+
+virtualenv --python=python3 venv
+source venv/bin/activate
+$PIPINSTALL -r /vagrant/requirements.txt -r /vagrant/requirements-proxy.txt
+
+if ! [ -d "chutney" ]; then
+	echo "Installing chutney"	
+	git clone https://github.com/torproject/chutney.git
+	cp /vagrant/tools/chutney/small-chut chutney
+	cd chutney
+	git apply /vagrant/tools/chutney/sandbox_patch
+else
+	echo "Chutney is already installed"
+fi
+
+echo "Installing TOR client"
+$APTGETINSTALL tor openjdk-11-jre
