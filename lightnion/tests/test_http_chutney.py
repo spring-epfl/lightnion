9731291405048fad1cf4d7e4517809b3c65d6390
chia-an <chia-an.yu@epfl.ch>
2018-11-28 20:55:39 +0000

    add functions for stress test

--- lightnion/tests/test_http_chutney.py
+++ lightnion/tests/test_http_chutney.py
@@ -1,6 +1,8 @@
 import lightnion as lnn
 import lightnion.http
 
+import pytest
+
 
 def test_polling():
     endpoint = ('127.0.0.1', 4990)
@@ -24,33 +26,102 @@ def test_polling():
     assert True
 
 
-def test_websocket():
-    method = 'websocket'
-    nb_downloads = 3
-    host, port = '127.0.0.1', 4990
-    padding = 100
+@pytest.fixture()
+def websocket_stress_test():
+    def _websocket_stress_test(nb_downloads=3,
+        padding=100, n_clients=1):
 
-    io_used = dict(
-        polling=lnn.http.polling.io,
-        websocket=lnn.http.websocket.io)[method]  # or 'polling'
+        method = 'websocket'
+        host, port = '127.0.0.1', 4990
 
-    endpoint = host, port
-    state, channel = lnn.http.client(*endpoint, io=io_used)
+        io_used = dict(
+            polling=lnn.http.polling.io,
+            websocket=lnn.http.websocket.io)[method]  # or 'polling'
 
-    # Send bunch of padding
-    for i in range(padding):
-        state = lnn.hop.send(state, lnn.cell.relay.cmd.RELAY_DROP)
+        endpoint = host, port
+        clients = []
+        for _ in range(n_clients):
+            state, channel = lnn.http.client(*endpoint, io=io_used)
+            clients.append(state)
 
-    for _ in range(nb_downloads):
-        # Retrieve something
-        state, authority = lnn.descriptors.download_authority(state)
+        # Send bunch of padding
+        for i in range(padding):
+            for j, state in enumerate(clients):
+                state = lnn.hop.send(state, lnn.cell.relay.cmd.RELAY_DROP)
+                clients[j] = state
 
-        # Retrieve something heavier
-        state, _ = lnn.hop.directory_query(state,
-            '/tor/status-vote/current/consensus',
-            compression='identity')  # (no cache nor parsing nor compression)
+        for _ in range(nb_downloads):
+            for j, state in enumerate(clients):
+                # Retrieve something
+                state, authority = lnn.descriptors.download_authority(state)
 
-    # Destroy the channel
-    state.link.close()
+                # Retrieve something heavier
+                state, _ = lnn.hop.directory_query(state,
+                    '/tor/status-vote/current/consensus',
+                    compression='identity')  # (no cache nor parsing nor compression)
 
+                clients[j] = state
+
+        # Destroy the channel
+        for state in clients:
+            state.link.close()
+
+    return _websocket_stress_test
+
+
+def test_websocket(websocket_stress_test):
+    websocket_stress_test()
+    assert True
+
+
+def test_websocket_create_10_client(websocket_stress_test):
+    websocket_stress_test(n_clients=10, nb_downloads=0,
+        padding=0)
+    assert True
+
+
+def test_websocket_create_30_client(websocket_stress_test):
+    websocket_stress_test(n_clients=30, nb_downloads=0,
+        padding=0)
+    assert True
+
+
+def test_websocket_10_client(websocket_stress_test):
+    websocket_stress_test(n_clients=10)
+    assert True
+
+
+def test_websocket_30_client(websocket_stress_test):
+    websocket_stress_test(n_clients=30)
+    assert True
+
+
+def test_websocket_1000_padding(websocket_stress_test):
+    websocket_stress_test(padding=1000)
+    assert True
+
+
+def test_websocket_10000_padding(websocket_stress_test):
+    websocket_stress_test(padding=10000)
+    assert True
+
+
+def test_websocket_10_downloads(websocket_stress_test):
+    websocket_stress_test(nb_downloads=10)
+    assert True
+
+
+def test_websocket_30_downloads(websocket_stress_test):
+    websocket_stress_test(nb_downloads=30)
+    assert True
+
+
+def test_websocket_100_downloads(websocket_stress_test):
+    websocket_stress_test(nb_downloads=100)
+    assert True
+
+
+# @pytest.mark.skip(reason="very slow, can be too slow sometimes, could be a problem")
+def test_websocket_1000_downloads(websocket_stress_test):
+    websocket_stress_test(nb_downloads=1000)
     assert True
