439579252661d1b52b651fb143b0f0fdb333a8c7
somecookie <ricardo.ferreiraribeiro@epfl.ch>
2018-12-15 17:16:02 +0100

    Implement a tool to download the chutney's key

--- tools/keys.py
+++ tools/keys.py
@@ -1,9 +1,9 @@
+import os
 import random
 import requests
 from Crypto.PublicKey import RSA
 import json as js
-
-# TODO: adapt this in order to use it with Chutney
+import re
 
 # Those are the IP's addresses of the 9 authorities
 ips = ['171.25.193.9:443', '86.59.21.38', '199.58.81.140', '194.109.206.212', '204.13.164.118',
@@ -74,22 +74,51 @@ def parse_signing_keys(raw):
 def get_signing_keys_info(path="tools/authority_signing_keys.json"):
     """
     Get the information of the authority router keys and save it to a json file.
-    Each key is saved as:
 
+
+    :param path: where we want to save the json
+    """
+    keys_dict = download_signing_keys()
+    if keys_dict is None:
+        raise ValueError("Error occurred during download of the keys")
+
+    to_json(keys_dict, path)
+
+
+def get_chutney_keys_info(saving_path="tools/chutney_authority_signing_keys.json"):
+    """
+    This function scrap the chutney's authority keys
+    :param saving_path: where we want to save the json
+    """
+    rootdir = "/home/vagrant/chutney/net/nodes"
+    keys = {}
+    for d in os.listdir(rootdir):
+        pattern = re.compile('\d*a')
+        if pattern.match(d) is not None:
+            subdir = rootdir + "/" + d
+            with open(subdir + "/fingerprint", "r") as file1:
+                fingerprint = file1.read().split(" ")[1][:-1]
+            with open(subdir + "/keys/authority_signing_key", "r") as file2:
+                key = file2.read()[:-1]
+
+            keys[fingerprint] = key
+    to_json(keys, saving_path)
+
+
+def to_json(keys_dict, path):
+    """
+    This function saves a mapping from fingerprint to PEM format keys on disk as a json where each key is saved as:
     fingerprint:{
         pem: key_pem            //the key in pem format
         modulus: modulus        //the modulus of the key
         exponent: exponent      //the exponent of the key
     }
 
-    :param path: where we want to save the json
+    :param keys_dict:
+    :param path:
+    :return:
     """
-    keys_dict = download_signing_keys()
     info = {}
-
-    if keys_dict is None:
-        raise ValueError("Error occurred during download of the keys")
-
     for fingerprint in keys_dict.keys():
         key_pem = keys_dict[fingerprint]
         key = RSA.importKey(key_pem)
@@ -105,7 +134,6 @@ def get_signing_keys_info(path="tools/authority_signing_keys.json"):
         info[fingerprint] = sub_dict
 
     info_json = js.dumps(info)
-
     with open(path, "w") as file:
         file.write(info_json)
 
