4ee7dfa5930f9ac8395b7be3385b6e93cd102fa7
Laurent Girod <laurent.girod@epfl.ch>
2020-04-15 17:15:42 +0200

    Remove deprecated submodules from __init__.

--- lightnion/__init__.py
+++ lightnion/__init__.py
@@ -2,12 +2,6 @@ import lightnion
 
 import lightnion.constants
 import lightnion.crypto
-import lightnion.cell
-
-import lightnion.cache
-
-import lightnion.create
-import lightnion.onion
 
 import lightnion.consensus
 import lightnion.descriptors
